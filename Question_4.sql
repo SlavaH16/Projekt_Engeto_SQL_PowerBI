2f31b41ffc33359d4e4ba88cb80a7b3b555a21db
SlavaH16 <slava.89@seznam.cz>
2024-04-16 22:39:53 +0200

    work version Que 4

---
+++ Question_4.sql
@@ -0,0 +1,18 @@
+
+-- Meziroční nárůsty/poklesy ve sledovaném období 2006-2018, dle druhu potravin --
+
+SELECT
+	tmh1.payroll_year,
+	tmh1.food_category,
+	tmh1.avg_price_per_year AS avg_price_first_year,
+	tmh2.avg_price_per_year AS avg_price_next_year,
+	round((tmh2.avg_price_per_year - tmh1.avg_price_per_year)/tmh1.avg_price_per_year),4)*100 AS year_increase
+FROM t_miloslav_houska_project_sql_primary_final tmh1 
+JOIN t_miloslav_houska_project_sql_primary_final tmh2
+   		ON tmh1.food_category = tmh2.food_category  
+   		AND tmh1.payroll_year = tmh2.payroll_year - 1
+WHERE tmh1.food_category IS NOT NULL 
+GROUP BY tmh1.food_category
+ORDER BY tmh1.payroll_year,tmh1.food_category  ;
+
+
