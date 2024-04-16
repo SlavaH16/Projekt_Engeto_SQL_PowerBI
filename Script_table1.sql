10e33f80a3c2f8e5a2285420645c3d9e637c9c35
SlavaH16 <slava.89@seznam.cz>
2024-04-16 11:33:57 +0200

    new version Script_table

--- Script_table1.sql
+++ Script_table1.sql
@@ -3,14 +3,21 @@
 
 CREATE TABLE t_miloslav_houska_project_sql_primary_final AS
     SELECT
-    cpib.name AS industry_branch_name,
     cp.payroll_year,
-    round(avg(cp.value),0) AS avg_payroll_per_year
+    cpib.name AS industry_branch_name,
+    round(avg(cp.value),0) AS avg_payroll_per_year,
+    cpwc.name_category,
+    round(avg(cpwc.value),2) AS avg_price_per_year,
+    cpwc.price_value,
+    cpwc.price_unit
     FROM czechia_payroll cp
     	JOIN czechia_payroll_industry_branch cpib 
-    	ON cp.industry_branch_code = cpib.code 
+    	ON cp.industry_branch_code = cpib.code
+    	LEFT JOIN czechia_price_with_category cpwc 
+    	ON cp.payroll_year = YEAR (cpwc.date_from)
     WHERE value_type_code = '5958'
-    GROUP BY payroll_year, name
-    ORDER BY name, payroll_year;
-    
+    GROUP BY cp.payroll_year, cp.industry_branch_code, cpwc.name_category
+    ORDER BY cp.payroll_year, cpib.name, cpwc.name_category;
    
+   
+
